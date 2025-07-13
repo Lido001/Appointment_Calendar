f2e30740949ae8a86b0f4d60465ad08707b450b0
Lido001 <146964211+Lido001@users.noreply.github.com>
2025-07-13 20:18:28 +0530

    build commit 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  build: {
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      treeshake: false, // <-- helps expose the actual file causing it
+    },
+  },
 })
